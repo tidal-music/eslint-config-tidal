333def08a2af20a2edee273a12d46015aaf0f525
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2016-10-03 14:22:32 +0200

    Remove ecmaFeatures from default

--- index.js
+++ index.js
@@ -2,24 +2,6 @@
 
 module.exports = {
   extends: 'xo',
-  ecmaFeatures: {
-    "arrowFunctions": true,
-    "destructuring": true,
-    "classes": true,
-    "defaultParams": true,
-    "blockBindings": true,
-    "modules": true,
-    "objectLiteralComputedProperties": true,
-    "objectLiteralShorthandMethods": true,
-    "objectLiteralShorthandProperties": true,
-    "restParams": true,
-    "spread": true,
-    "forOf": true,
-    "generators": true,
-    "templateStrings": true,
-    "superInFunctions": true,
-    "experimentalObjectRestSpread": true
-  },
   rules: {
     'indent': [
       2,
