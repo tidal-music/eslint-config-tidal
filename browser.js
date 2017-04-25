4dfe34726400a7a1774b410138a6e00470e1a895
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2017-04-25 14:29:45 +0200

    Fix accidental tabs

--- browser.js
+++ browser.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   extends: path.join(__dirname, 'index.js'),
   env: {
-		node: false,
+    node: false,
     es6: false,
-		browser: true
-	}
+    browser: true
+  }
 };
