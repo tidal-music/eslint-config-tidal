104e0bcd1bbda8e3f1cef6f87ff1efb2437af4eb
Koen Romers <koen.romers@tidal.com>
2020-05-28 15:02:04 +0200

    put back parserOptions in imports rule

--- rules/imports.js
+++ rules/imports.js
@@ -3,6 +3,10 @@ module.exports = {
   env: {
     es6: true,
   },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
   plugins: ['import'],
   settings: {
     'import/resolver': {
